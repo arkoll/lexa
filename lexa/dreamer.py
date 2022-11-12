a537121ec000b9a33d418f00ac9fb9f41634b738
arkoll <ar.ko.latyshev@gmail.com>
2022-11-12 22:19:02 +0300

    fix variables scope

--- lexa/dreamer.py
+++ lexa/dreamer.py
@@ -118,14 +118,15 @@ class Dreamer(tools.Module):
     self._mean_metrics.update_state(feat)
     metric_std = tf.keras.metrics.mean_squared_error(feat, self._mean_metrics.result())
     self._metrics['feat_std'].update_state(metric_std)
-    if self._should_res_met:
-        self._max_state = tf.identity(feat)
-        self._min_state = tf.identity(feat)
-        self._should_res_met = False
-    self._max_state = tf.math.maximum(feat, self._max_state)
-    self._min_state = tf.math.minimum(feat, self._min_state)
-    metric_div = tf.keras.metrics.mean_squared_error(self._min_state, self._max_state)
-    self._metrics['feat_div'].update_state(metric_div)
+    with tf.init_scope():
+      if self._should_res_met:
+          self._max_state = tf.identity(feat)
+          self._min_state = tf.identity(feat)
+          self._should_res_met = False
+      self._max_state = tf.math.maximum(feat, self._max_state)
+      self._min_state = tf.math.minimum(feat, self._min_state)
+      metric_div = tf.keras.metrics.mean_squared_error(self._min_state, self._max_state)
+      self._metrics['feat_div'].update_state(metric_div)
     if not training:
       goal = self._wm.get_goal(obs, training=False)
       if self._config.offpolicy_opt:
